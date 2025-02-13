10659a96fa9b970cc0050196fe48c71480d4d176
Jemzkie <jemrowynn506@gmail.com>
2025-02-13 14:18:12 +0800

    Register Updated & Added Dashboard

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -8,7 +8,7 @@ import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 
 const mapContainerStyle = {
   width: "100%",
-  height: "300px",
+  height: "500px",
 };
 
 const center = {
@@ -51,7 +51,8 @@ const Register = () => {
     try {
       // 1. Get the formatted address (fallback if no hospital is found)
       const geocodeResponse = await geocoder.geocode({ location: latLng });
-      const formattedAddress = geocodeResponse.results[0]?.formatted_address || "";
+      const formattedAddress =
+        geocodeResponse.results[0]?.formatted_address || "";
 
       // 2. Search for hospitals near the clicked location
       const placesResponse = await new Promise((resolve, reject) => {
@@ -126,7 +127,11 @@ const Register = () => {
     }
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
 
       // Store additional data in Firestore
@@ -153,125 +158,141 @@ const Register = () => {
   };
 
   return (
-    <div className="w-full h-auto overflow-y py-25 flex flex-col justify-center items-center gap-15 bg-gradient-to-b to-[#F5EFE8] from-[#d5e8d4] relative">
+    <div className="w-full h-auto overflow-y py-25 px-8 flex flex-col  gap-15 bg-gradient-to-b to-[#F5EFE8] from-[#d5e8d4] relative">
       {/* Header */}
-      <div className="absolute w-[300px] h-15 border border-white rounded-full bg-[#d5e8d4] shadow-black drop-shadow-xl top-6 justify-center items-center flex ">
-        <div className="flex items-center justify-center">
-          <img src={Logo} alt="React logo" width="50" height="50" />
-          <label className="font-medium text-3xl font-mono">NeoCare</label>
+      <div className="w-full h-auto flex justify-center items-center">
+        <div className="w-[300px] h-15 border border-white rounded-full bg-[#d5e8d4] shadow-black drop-shadow-xl justify-center items-center flex">
+          <div className="flex items-center justify-center">
+            <img src={Logo} alt="React logo" width="50" height="50" />
+            <label className="font-medium text-3xl font-mono">NeoCare</label>
+          </div>
         </div>
       </div>
 
-      <div className="w-[600px] h-auto p-8 flex gap-2 flex-col">
-        <label>Email</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="Enter Email"
-          type="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Username</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="Enter Username"
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label>Name</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="Enter Name"
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label>Photo URL</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="Enter Photo URL"
-          type="text"
-          onChange={(e) => setPhotoUrl(e.target.value)}
-        />
-        <label>Password</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="Enter Password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label>Specialty</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="Enter your Specialty"
-          type="text"
-          onChange={(e) => setSpecialty(e.target.value)}
-        />
+      <div className="w-full flex flex-row gap-2">
+        <div className="w-1/2 h-auto p-8 flex gap-2 flex-col">
+          <label>Email</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="Enter Email"
+            type="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Username</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="Enter Username"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label>Name</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="Enter Name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label>Photo URL</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="Enter Photo URL"
+            type="text"
+            onChange={(e) => setPhotoUrl(e.target.value)}
+          />
+          <label>Password</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="Enter Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label>Specialty</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="Enter your Specialty"
+            type="text"
+            onChange={(e) => setSpecialty(e.target.value)}
+          />
 
-        {/* Appointment Details */}
-        <label>Available Days</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="e.g., Monday to Friday"
-          type="text"
-          onChange={(e) => setAvailableDays(e.target.value)}
-        />
-        <label>Consultation Hours</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="e.g., 9:00 AM to 5:00 PM"
-          type="text"
-          onChange={(e) => setConsultationHours(e.target.value)}
-        />
-        <label>Platform (In-person/Online)</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="e.g., In-person, Online, or Both"
-          type="text"
-          onChange={(e) => setPlatform(e.target.value)}
-        />
-        <label>Contact Information (Phone Number)</label>
-        <input
-          className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
-          placeholder="Enter Phone Number"
-          type="text"
-          onChange={(e) => setContactInfo(e.target.value)}
-        />
-
-        {/* Map for selecting hospital location */}
-        <label>Hospital Location</label>
-        {isLoaded ? (
-          <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            zoom={10}
-            center={center}
-            onClick={handleMapClick}
+          {/* Appointment Details */}
+          <label>Available Days</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="e.g., Monday to Friday"
+            type="text"
+            onChange={(e) => setAvailableDays(e.target.value)}
+          />
+          <label>Consultation Hours</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="e.g., 9:00 AM to 5:00 PM"
+            type="text"
+            onChange={(e) => setConsultationHours(e.target.value)}
+          />
+          <label>Platform (In-person/Online)</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="e.g., In-person, Online, or Both"
+            type="text"
+            onChange={(e) => setPlatform(e.target.value)}
+          />
+          <label>Contact Information (Phone Number)</label>
+          <input
+            className="w-full h-10 rounded-xl border-1 border-[#6bc4c1] bg-white px-4"
+            placeholder="Enter Phone Number"
+            type="text"
+            onChange={(e) => setContactInfo(e.target.value)}
+          />
+          <button
+            onClick={register}
+            className="w-full h-10 rounded-xl bg-[#6bc4c1] text-white font-medium text-xl mt-5 font-mono cursor-pointer duration-300 hover:bg-[#48817f]"
           >
-            {hospitalLocation && <Marker position={hospitalLocation} />}
-          </GoogleMap>
-        ) : (
-          <p>Loading map...</p>
-        )}
+            SIGN UP
+          </button>
+          <div className="flex-col flex justify-center items-center gap-5">
+            <label>
+              Already have an Account?{" "}
+              <Link to="/" className="underline">
+                Sign In!
+              </Link>
+            </label>
+          </div>
+        </div>
 
-        {hospitalLocation && (
-          <p>
-            Selected Location: Latitude: {hospitalLocation.lat}, Longitude: {hospitalLocation.lng}
-          </p>
-        )}
-        {hospitalAddress && <p>Hospital Address: {hospitalAddress}</p>}
+        <div className="w-1/2 flex items-center justify-center">
+          <div className="w-full">
+            {/* Map for selecting hospital location */}
+            <label>Hospital Location</label>
+            {isLoaded ? (
+              <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={10}
+                center={center}
+                onClick={handleMapClick}
+              >
+                {hospitalLocation && <Marker position={hospitalLocation} />}
+              </GoogleMap>
+            ) : (
+              <p>Loading map...</p>
+            )}
 
-        <input
-          type="text"
-          placeholder="Hospital Name (auto-filled)"
-          value={hospitalAddress}
-          onChange={(e) => setHospitalAddress(e.target.value)}
-          readOnly // Optional: Prevent manual edits if you want
-        />
+            {hospitalLocation && (
+              <p>
+                Selected Location: Latitude: {hospitalLocation.lat}, Longitude:{" "}
+                {hospitalLocation.lng}
+              </p>
+            )}
+            {hospitalAddress && <p>Hospital Address: {hospitalAddress}</p>}
 
-        <button
-          onClick={register}
-          className="w-full h-10 rounded-xl bg-[#6bc4c1] text-white font-medium text-xl mt-5 font-mono cursor-pointer duration-300 hover:bg-[#48817f]"
-        >
-          SIGN UP
-        </button>
+            <input
+              type="text"
+              placeholder="Hospital Name (auto-filled)"
+              value={hospitalAddress}
+              onChange={(e) => setHospitalAddress(e.target.value)}
+              readOnly // Optional: Prevent manual edits if you want
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
